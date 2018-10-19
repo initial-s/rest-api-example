7199e96bf3a5c6c55a15349b9c6440a3c8da0759
initial-s <32444534+initial-s@users.noreply.github.com>
2018-10-19 17:43:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from datetime import datetime
 import requests, json
 app = Flask(__name__)
 
-@app.route('/') #https://yourdomain.com/
+@app.route('/citl_design') #https://yourdomain.com/
 def homepage():
     return '''<!DOCTYPE html>
 <html>
 <head>
-<title>arsybai-api</title>
+<title>initial_S-Test-api</title>
 </head>
 <body>
-<h1>Arsybai API</h1>
+<h1>Initial_S Test API</h1>
 <h2>Add me</h2>
-<div class="line-it-button" data-lang="en" data-type="friend" data-lineid="arsy22bai" style="display: none;"></div>
+<div class="line-it-button" data-lang="en" data-type="friend" data-lineid="devilblack86" style="display: none;"></div>
  <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
 </body>
 </html>'''
@@ -24,7 +24,7 @@ def hello(name):
     
 @app.route('/json?') #https://yourdomain.com/json?
 def out():
-    contoh = ['anu','anu1','anu2']
+    contoh = ['citl','design','kreasi tanpa batas']
     data = {
         'status':'OK',
         'result':contoh
