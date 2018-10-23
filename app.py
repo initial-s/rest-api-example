188ab3581ec0ea5c1af25000857394c41489dbd0
initial-s <32444534+initial-s@users.noreply.github.com>
2018-10-23 22:20:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -103,19 +103,11 @@ def instapost(usn):
                 datas.append({'url':url,'vid':video})
     return(json.dumps(datas, indent=4, sort_keys=False))
 @app.route('/template' ,methods=['POST'])
-def out(title, columns):
-   # test = [{"type": "template","altText": "testing","template": {"type": "image_carousel","columns": [{"imageUrl": "https://image.ibb.co/b9JR5p/20180811_194145.png","action": {"type": "uri","uri": "http://line.me/ti/p/~devilblack86","area": {"x": 520,"y": 0,"width": 520,"height": 1040}}}]}}]
+def out():
+    test = [{"type": "template","altText": "testing","template": {"type": "image_carousel","columns": [{"imageUrl": "https://image.ibb.co/b9JR5p/20180811_194145.png","action": {"type": "uri","uri": "http://line.me/ti/p/~devilblack86","area": {"x": 520,"y": 0,"width": 520,"height": 1040}}}]}}]
     data = {
         'status':'OK',
-        'message': {
-            'type': 'template,
-            'altText': title,
-            'template': {
-                'type': 'image_carousel',
-                'columns': columns
-            }
-        }
-    }
+        'message': test
     return(json.dumps(data, indent=4, sort_keys=False))
 
 if __name__ == '__main__':
